15a7edfb74135cb4d40e725a9a2d3f0b97b66dd9
BimalKafle <56013956+BimalKafle@users.noreply.github.com>
2025-03-22 18:03:58 +0900

    completed player and config

--- flappy/player.py
+++ flappy/player.py
@@ -1,5 +1,12 @@
 import pygame
-from flappy.config import GROUNDY
+from flappy.config import (
+    GROUNDY,
+    PLAYER_VELOCITY_Y,
+    PLAYER_MAX_VELOCITY_Y,
+    PLAYER_MIN_VELOCITY_Y,
+    PLAYER_ACC_Y,
+    PLAYER_FLAP_ACC_V
+)
 
 class Player:
     def _init_(self,x,y,sprites,sounds):
@@ -15,4 +22,31 @@ class Player:
         self.sprites=sprites
         self.sounds=sounds
 
-        
+        self.vel_y=PLAYER_VELOCITY_Y
+        self.max_vel_y=PLAYER_MAX_VELOCITY_Y
+        self.min_vel_y=PLAYER_MIN_VELOCITY_Y
+        self.acc_y=PLAYER_ACC_Y
+        self.flap_accv=PLAYER_FLAP_ACC_V
+
+        self.flapped=false
+
+    def flap(self):
+        self.vel_y=self.flap_accv
+        self.flapped=True
+        self.sounds['wing'].play()
+
+    def move(self):
+        if(self.vel_y<self.max_vel_y and not self.flapped):
+            self.vel_y+=self.acc_y
+        if self.flapped:
+            self.flapped=False 
+
+        player_height=self.sprites['player'].get_height()
+        self.y+=min(self.vel_y,GROUNDY-self.y-player_height)
+
+
+    def draw(self,screen):
+        screen.blite(self.sprites['player'],(self.x,self.y))
+
+    def get_rect(self):
+        return self.sprites['player'].get_rect(topleft=(self.x,self.y)) 
